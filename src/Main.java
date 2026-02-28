06366d8d4639da1b6cf759459597c23ebc2b88ad
julianamoreno <jmorenof@ucundinamarca.edu.co>
2026-02-27 22:32:26 -0500

    actulizacion por separado con listas

--- src/Main.java
+++ src/Main.java
@@ -1,85 +1,93 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 void main() {
     Scanner teclado = new Scanner(System.in);
 
-    //OBJETO CREADO DESDE EL CONSTRUCTOR VACIO
-    Mascota pet = new Mascota();
-
-    pet.setNOMBRE("PEPITO");
-    pet.setESPECIE("PEPITO");
-    pet.setEDAD(00);
-    pet.setPESO(00.0);
-    pet.setVACUNADO(false);
-    System.out.println(pet.toString());
-
-    //CREAR OBJETOS CON LOS DATOS INGRESADOS DEL USUARIO
-    // SOLO SE CREA UN OBJETO, POR LO TANTO ESTWE YA NO ES NECESARIO, Mascota pet = new Mascota();
-    System.out.println("INGRESE EL NOMBRE DE LA MASCOTA: ");
-    pet.setNOMBRE(teclado.next());
-
-    System.out.println("INGRESE LA ESPECIE DE LA MASCOTA: ");
-    pet.setESPECIE(teclado.next());
-
-    System.out.println("INGRESE LA EDAD DE LA MASCOTA: ");
-    pet.setEDAD(teclado.nextInt());
-
-    System.out.println("INGRESE EL PESO DE LA MASCOTA: ");
-    pet.setPESO(teclado.nextDouble());
-
-    //PARA INGRESAR LA RESPUESTA DE VACUNA RESPONDER SOLO TRUE O FALSE O DA ERROR
-    System.out.println("INGRESE SI LA MASCOTA ES VACUNADA: ");
-    pet.setVACUNADO(teclado.nextBoolean());
-
-    System.out.println(pet.toString());
-
     //MENU CON DO-WHILE
     //EL DO-WHILE SE REPITE HASTA QUE OPCIÓN SEA 6
+    List<Mascota>ltsMascota = new ArrayList<>();
     int OPCIONES;
+    int OPCIONES1;
     do {
-        System.out.println("         MENÚ VETERINARIA               ");
-        System.out.println("1. MOSTRAR INFORMACIÓN");
-        System.out.println("2. ALIMENTAR");
-        System.out.println("3. VACUNAR");
-        System.out.println("4. CUMPLIR AÑOS");
-        System.out.println("5. MOSTRAR INFORMACIÓN FINAL");
-        System.out.println("6. SALIR");
+        System.out.println("   MENÚ VETERINARIA               ");
+        System.out.println("1. CREAR MASCOTA");
+        System.out.println("2. MOSTRAR TODO");
+        System.out.println("3. BUSCAR POR NOMBRE");
+
         System.out.print("INGRESE UNA OPCIÓN: ");
         OPCIONES = teclado.nextInt();
 
         switch (OPCIONES) {
-            case 1:
-                pet.MOSTRAR_INFORMACION();
-                break;
+            case 1: {
+                System.out.println("INGRESE EL NOMBRE DE LA MASCOTA");
+                String NOMBRE = teclado.next();
+                System.out.println("INGRESE LA ESPECIE DE LA MASCOTA");
+                String ESPECIE = teclado.next();
+                System.out.println("INGRESE LA EDAD DE LA MASCOTA");
+                int EDAD = teclado.nextInt();
+                System.out.println("INGRESE LA MASCOTA ESTA VACUNADA");
+                Boolean VACUNADO = teclado.nextBoolean();
+                System.out.println("INGRESE EL PESO DE LA MASCOTA");
+                double PESO = teclado.nextDouble();
 
-            case 2:
-                System.out.print("INGRESE LA CANTIDAD DE ALIMENTO EN KG: ");
-                double cantidad = teclado.nextDouble();
-                pet.ALIMENTAR(cantidad);
+                Mascota m1 = new Mascota (NOMBRE, ESPECIE, EDAD, VACUNADO, PESO);
+                ltsMascota.add(m1);
                 break;
+            }
+            case 2: {
+                System.out.println(ltsMascota);
+                for (Mascota m : ltsMascota)
+                    System.out.println(m);
 
-            case 3:
-                pet.VACUNAR();
                 break;
+            }
+            case 3: {
+                String nombremascota;
+                System.out.println("BUSCAR POR NOMBRE");
+                System.out.println("INGRESE EL NOMBRE A BUSCAR");
+                nombremascota = teclado.next();
 
-            case 4:
-                pet.CUMPLIR_AÑOS();
-                break;
+                Mascota masc1 = null;
 
-            case 5:
-                pet.MOSTRAR_INFORMACION();
-                System.out.println("INFORMACIÓN FINAL MOSTRADA");
-                break;
+                for (Mascota m : ltsMascota){
+                    if (m.getNOMBRE().equalsIgnoreCase(nombremascota)){
+                        masc1 = m;
+                    }
+                }
 
-            case 6:
-                System.out.println("PROGRAMA FINALIZADO");
-                break;
+                System.out.println(masc1.toString());
 
-            default:
-                System.out.println("ERROR, INGRESE UNA OPCION");
+                    System.out.println("""
+                    SUBMENÚ VETERINARIA
+                    1. ALIMENTAR MASCOTA
+                    2. VACUNAR MASCOTA
+                    3. CUMPLIR AÑOS
+                    """);
+
+                    System.out.print("INGRESE UNA OPCIÓN: ");
+                    OPCIONES1 = teclado.nextInt();
+
+                switch (OPCIONES1) {
+                case 1:
+                    System.out.print("INGRESE LA CANTIDAD DE ALIMENTO EN KG: ");
+                    double cantidad = teclado.nextDouble();
+                    masc1.ALIMENTAR(cantidad);
+                    break;
+                case 2:
+                    masc1.VACUNAR();
+                    System.out.println("VACUNADO");
+                    break;
+
+                case 3:
+                    masc1.CUMPLIR_AÑOS();
+                    break;
+                }
+            }
+
         }
 
-    } while (OPCIONES != 6);
+    } while (OPCIONES != 3);
 
     teclado.close();
 
